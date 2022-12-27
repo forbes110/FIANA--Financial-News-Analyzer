579f235c517ffea14c2661a090e8b3753791abc2
chung-han chiang <eayaps@gmail.com>
2022-12-27 20:48:09 +0800

    update tain.sh

--- models/domain_cls/scripts/train.sh
+++ models/domain_cls/scripts/train.sh
@@ -3,14 +3,13 @@ python run_domain_cls.py \
     --do_train \
     --do_eval \
     --model_name_or_path luhua/chinese_pretrain_mrc_macbert_large \
-    --tokenizer_name TsinghuaAI/CPM-Generate \
     --max_seq_length 128 \
     --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine_with_restarts \
     --weight_decay 1e-5 \
     --learning_rate 5e-5 \
-    --num_train_epochs 10 \
+    --num_train_epochs 5 \
     --output_dir ./ckpt_save \
     --output_file ./pred_domnain_1.csv \
     --overwrite_output_dir
